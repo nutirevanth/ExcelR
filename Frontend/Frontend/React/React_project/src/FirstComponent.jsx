782531b4e7aeff1401fc5b905b5e2c7be344ea16
Revanth <Revanth@gmail.com>
2025-04-30 20:03:14 +0530

    React

--- Frontend/Frontend/React/React_project/src/FirstComponent.jsx
+++
@@ -1,8 +0,0 @@
-const First=()=>{
-    return(
-        <>
-        <h1>First Component</h1>
-        </>
-    );
-}
-export default First;
--- Frontend/backend/Largestnum.java
+++
@@ -1,9 +0,0 @@
-public class Largestnum
-{
-	public static void main(String[] args) {
-	    int a=30;
-	    int b=80;
-	    int largest=(a>b)?a:b;
-		System.out.println("the largest number is "+largest);
-	}
-}
