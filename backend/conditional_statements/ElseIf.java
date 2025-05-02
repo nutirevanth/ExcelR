3133c2b8224df27539cc05dd7c4f63c654cbe275
Revanth <Revanth@gmail.com>
2025-05-02 10:50:02 +0530

    java notes

---
+++ backend/conditional_statements/ElseIf.java
@@ -0,0 +1,18 @@
+package conditional_statements;
+
+public class ElseIf {
+    public static void main(String[] args) {
+            int num=24;
+            if(num%2==0){
+                System.out.println("Even");
+                System.out.println("Even");
+            }
+            // System.err.println();
+            else{
+                System.out.println("Odd");
+            }
+            // else{
+            // }
+        }
+    
+}
